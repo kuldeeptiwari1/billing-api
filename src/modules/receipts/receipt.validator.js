66d28bcb4af5b7b81e5065b8937ef5fc027df54b
Gopal Thakur <gopal.thakur@ssaihq.com>
2025-05-06 23:09:12 +0530

    add login data

--- src/modules/receipts/receipt.validator.js
+++ src/modules/receipts/receipt.validator.js
@@ -13,147 +13,67 @@ const options = {
 module.exports = {
   add: (httpRequest) => {
     const schema = Joi.object({
-      receiptDate: Joi.date()
-        .messages({ 'date.base': 'must be a valid date', 'any.required': 'is required' })
-        .required(),
-      studentName: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      phone: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      email: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      course: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      totalAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paidReceiptAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paidAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      remainingAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      fineAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      selectSeries: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPerson1: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonNo1: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonEmail1: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPerson2: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonNo2: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonEmail2: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paymentMode: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      bankType: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      paytmAccount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      feesDueDate: Joi.date()
-        .messages({ 'date.base': 'must be a valid date', 'any.required': 'is required' })
-        .required(),
-      bookIssue: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      bookCode: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      ReceiptType: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paymentDetails: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      dueDate: Joi.date().messages({ 'date.base': 'must be a valid date', 'any.required': 'is required' }).required(),
-      notes: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      isDeleted: Joi.boolean()
-        .messages({ 'boolean.base': 'must be true or false', 'any.required': 'is required' })
-        .default(false)
+      receiptDate: Joi.date().required(),
+      studentName: Joi.string().required(),
+      phone: Joi.string().required(),
+      email: Joi.string().required(),
+      course: Joi.string().required(),
+      totalAmount: Joi.string().required(),
+      paidReceiptAmount: Joi.string().required(),
+      paidAmount: Joi.string().required(),
+      remainingAmount: Joi.string().required(),
+      fineAmount: Joi.string().required(),
+      selectSeries: Joi.string().required(),
+      contactPerson1: Joi.string().required(),
+      contactPersonNo1: Joi.string().required(),
+      contactPersonEmail1: Joi.string().required(),
+      contactPerson2: Joi.string().required(),
+      contactPersonNo2: Joi.string().required(),
+      contactPersonEmail2: Joi.string().required(),
+      paymentMode: Joi.string().required(),
+      bankType: Joi.string().required(),
+      paytmAccount: Joi.string().required(),
+      feesDueDate: Joi.date().required(),
+      bookIssue: Joi.string().required(),
+      bookCode: Joi.string().required(),
+      ReceiptType: Joi.string().required(),
+      paymentDetails: Joi.string().required(),
+      dueDate: Joi.date().required(),
+      notes: Joi.string().required(),
+      isDeleted: Joi.boolean().default(false)
     });
     return schema.validate(httpRequest.body, options);
   },
   update: (httpRequest) => {
     const schema = Joi.object({
-      receiptDate: Joi.date()
-        .messages({ 'date.base': 'must be a valid date', 'any.required': 'is required' })
-        .required(),
-      studentName: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      phone: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      email: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      course: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      totalAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paidReceiptAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paidAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      remainingAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      fineAmount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      selectSeries: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPerson1: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonNo1: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonEmail1: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPerson2: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonNo2: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      contactPersonEmail2: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paymentMode: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      bankType: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      paytmAccount: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      feesDueDate: Joi.date()
-        .messages({ 'date.base': 'must be a valid date', 'any.required': 'is required' })
-        .required(),
-      bookIssue: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      bookCode: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      ReceiptType: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      paymentDetails: Joi.string()
-        .messages({ 'string.base': 'must be a string', 'any.required': 'is required' })
-        .required(),
-      dueDate: Joi.date().messages({ 'date.base': 'must be a valid date', 'any.required': 'is required' }).required(),
-      notes: Joi.string().messages({ 'string.base': 'must be a string', 'any.required': 'is required' }).required(),
-      isDeleted: Joi.boolean()
-        .messages({ 'boolean.base': 'must be true or false', 'any.required': 'is required' })
-        .default(false)
+      receiptDate: Joi.date().required(),
+      studentName: Joi.string().required(),
+      phone: Joi.string().required(),
+      email: Joi.string().required(),
+      course: Joi.string().required(),
+      totalAmount: Joi.string().required(),
+      paidReceiptAmount: Joi.string().required(),
+      paidAmount: Joi.string().required(),
+      remainingAmount: Joi.string().required(),
+      fineAmount: Joi.string().required(),
+      selectSeries: Joi.string().required(),
+      contactPerson1: Joi.string().required(),
+      contactPersonNo1: Joi.string().required(),
+      contactPersonEmail1: Joi.string().required(),
+      contactPerson2: Joi.string().required(),
+      contactPersonNo2: Joi.string().required(),
+      contactPersonEmail2: Joi.string().required(),
+      paymentMode: Joi.string().required(),
+      bankType: Joi.string().required(),
+      paytmAccount: Joi.string().required(),
+      feesDueDate: Joi.date().required(),
+      bookIssue: Joi.string().required(),
+      bookCode: Joi.string().required(),
+      ReceiptType: Joi.string().required(),
+      paymentDetails: Joi.string().required(),
+      dueDate: Joi.date().required(),
+      notes: Joi.string().required(),
+      isDeleted: Joi.boolean().default(false)
     });
     return schema.validate(httpRequest.body, options);
   }
